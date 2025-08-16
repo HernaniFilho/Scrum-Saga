2b6baa1b8e84d06e3c01d14c21e3ecab4db606a1
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2025-08-16 14:01:13 -0300

    Tabuleiro centralizado

---
+++ Assets/Scripts/CenterBoard.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CenterBoard : MonoBehaviour
+{
+  private Camera playerCamera;
+
+  [SerializeField] private float spawnDistance = 297f;
+  [SerializeField] private float xOffset = 160f;
+  [SerializeField] private float yOffset = -20f;
+    
+  private void Start()
+  {
+    playerCamera = Camera.main;
+  }
+
+  private void Update()
+  {
+    Vector3 screenCenter = new Vector3(
+      Screen.width / 2f + xOffset,
+      Screen.height / 2f + yOffset,
+      spawnDistance
+    );
+    
+    transform.position = playerCamera.ScreenToWorldPoint(screenCenter);
+  }
+}
