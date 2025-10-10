6ef81b8874c7193d2d8d2d52b63048dd988c6e7f
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2025-10-10 15:20:22 -0300

    style: atualiza cores dos botões verdes e a cor do botão de forma quando selecionada

--- Assets/Scripts/MobilePortability/MobileScaler.cs
+++ Assets/Scripts/MobilePortability/MobileScaler.cs
@@ -62,6 +62,10 @@ public class MobileScaler : MonoBehaviour
             {
                 ManageMobile();
             }
+            else
+            {
+                ManageDesktop();
+            }
         }
     }
 
